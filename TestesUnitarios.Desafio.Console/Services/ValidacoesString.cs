c466b0bb4df6a519cb30e6bdd7fadad4509823af
Mirilice <mariaalicepco10@gmail.com>
2024-01-14 00:07:06 -0300

    correçoes

--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -2,19 +2,19 @@ namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesString
     {
-        public int RetornarQuantidadeCaracteres(string texto)
+        public int RetornarQuantidadeCaracteres(string texto)//quantidade de caracteres em string texto
         {
             var numeroCaracteres = texto.Length;
             return numeroCaracteres;
         }
 
-        public bool ContemCaractere(string texto, string textoProcurado)
+        public bool ContemCaractere(string texto, string textoProcurado)//true ou false se o trecho procurado existe no texto
         {
             var contem = texto.Contains(textoProcurado);
             return contem;
         }
 
-        public bool TextoTerminaCom(string texto, string textoProcurado)
+        public bool TextoTerminaCom(string texto, string textoProcurado)//true ou false se o trecho procurado existe no FINAL do texto, apenas
         {
             var termina = texto.EndsWith(textoProcurado);
             return termina;
